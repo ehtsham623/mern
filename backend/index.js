08e840045f2c9318b849196c76431f31950162a4
ehtsham623 <ehtshamulhaqat@gmail.com>
2024-12-02 22:01:53 +0500

    structure implemnted

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger);
 
-///routes
 app.use("/api/products", products);
-///routes
 
 app.use(notFoundHandler);
 app.use(errorHandler);
