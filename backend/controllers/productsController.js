e8c00d6057398f52992a9416a28ddc3f99381667
ehtsham623 <ehtshamulhaqat@gmail.com>
2024-12-03 10:04:25 +0500

    added asyn handler

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -1,3 +1,5 @@
+import asyncHandler from "express-async-handler";
+
 ///json example
 let products = [
   { id: "1", name: "data1" },
@@ -7,7 +9,7 @@ let products = [
 
 //@disc  get single product
 //@route GET /api/product/:id
-export const getSingleProduct = async (req, res, next) => {
+export const getSingleProduct = asyncHandler(async (req, res, next) => {
   const productId = req.params.id;
   const product = products.find((u) => u.id === productId);
   if (product) {
@@ -17,22 +19,22 @@ export const getSingleProduct = async (req, res, next) => {
     error.status = 404;
     return next(error);
   }
-};
+});
 
 //@disc  get all product or with limit throug query param
 //@route GET /api/product/
-export const getAllProducts = async (req, res, next) => {
+export const getAllProducts = asyncHandler(async (req, res, next) => {
   const limit = parseInt(req.query.limit);
   if (!isNaN(limit) && limit > 0) {
     res.status(200).json(products.slice(0, limit));
   } else {
     res.status(200).json(products);
   }
-};
+});
 
 //@disc  post new product
 //@route POST /api/product/
-export const postNewProduct = async (req, res, next) => {
+export const postNewProduct = asyncHandler(async (req, res, next) => {
   const newProduct = { id: products.length + 1, name: req.body.name };
   if (newProduct.name) {
     products.push(newProduct);
@@ -42,11 +44,11 @@ export const postNewProduct = async (req, res, next) => {
     error.status = 404;
     return next(error);
   }
-};
+});
 
 //@disc  put new product
 //@route PUT /api/product/
-export const updateProduct = async (req, res, next) => {
+export const updateProduct = asyncHandler(async (req, res, next) => {
   const productId = req.params.id;
   const product = products.find((u) => u.id === productId);
   if (product) {
@@ -57,11 +59,11 @@ export const updateProduct = async (req, res, next) => {
     error.status = 404;
     return next(error);
   }
-};
+});
 
 //@disc  delete product
 //@route DELETE /api/product/
-export const deleteProduct = async (req, res, next) => {
+export const deleteProduct = asyncHandler(async (req, res, next) => {
   const productId = req.params.id;
   const product = products.find((u) => u.id === productId);
   if (product) {
@@ -72,4 +74,4 @@ export const deleteProduct = async (req, res, next) => {
     error.status = 404;
     return next(error);
   }
-};
+});
