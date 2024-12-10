c18a4eb4e01eb1fb2bb0a0ec01f0cc0354307cae
ehtsham623 <ehtshamulhaqat@gmail.com>
2024-12-10 05:48:52 +0500

    added env

--- frontend/src/api/reqEndpoints.js
+++ frontend/src/api/reqEndpoints.js
@@ -1,13 +1,13 @@
 /* --- Server --- */
 
-export const DEV_SERVER = "http://localhost:4000";
+export const DEV_SERVER = process.env.REACT_APP_SERVER_URL;
 
 /* --- Requests --- */
 
 export const REQ = {
-  LOGIN: DEV_SERVER + "/login",
-  SIGNUP: DEV_SERVER + "/signup",
-  GET_USER: DEV_SERVER + "/getUser",
+  LOGIN: DEV_SERVER + "/users/login",
+  SIGNUP: DEV_SERVER + "/users/signup",
+  GET_USER: DEV_SERVER + "/users",
   PRODUCTS: {
     GET_PRODUCTS: DEV_SERVER + "/products",
   },
--- frontend/src/pages/auth/signUpPage.jsx
+++ frontend/src/pages/auth/signUpPage.jsx
@@ -10,6 +10,12 @@ const SignUpPage = () => {
       </span>
       <div className="border relative px-4 pt-7 pb-8 bg-white shadow-xl w-1/2 max-w-md mx-auto sm:px-10 rounded-b-md">
         <form action="">
+          <label className="block pb-1">Name</label>
+          <input
+            type="text"
+            className="border w-full h-10 px-3 mb-5 rounded-md"
+            placeholder="Name"
+          />
           <label className="block pb-1">Email</label>
           <input
             type="Email"
@@ -22,24 +28,7 @@ const SignUpPage = () => {
             className="border w-full h-10 px-3 mb-5 rounded-md"
             placeholder="password"
           />
-          <div className="flex items-start">
-            <div className="flex items-start">
-              <div className="flex items-center">
-                <input
-                  id="remember"
-                  aria-describedby="remember"
-                  type="checkbox"
-                  className="bg-gray-50 border border-gray-300 focus:ring-3 focus:ring-blue-300 h-4 w-4 rounded dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-                  required=""
-                />
-              </div>
-              <div className="text-sm ml-3">
-                <label for="remember" className="font-medium text-gray-900">
-                  Remember me
-                </label>
-              </div>
-            </div>
-          </div>
+
           <div className="flex justify-between items-center">
             <button className="mt-5 bg-green-500 text-white uppercase text-sm font-semibold px-14 py-3 rounded">
               Signup
